d1cdca5d43f30c51ffaba3467b2f6c3a9ce43e22
shulamitmelamed <shulamitmelamed9188@gmail.com>
2023-05-14 16:56:34 +0300

    checking the json

--- NodeExpress/index.js
+++ NodeExpress/index.js
@@ -2,11 +2,11 @@ import express from "express";
 import apikey from './apikey.json' assert { type: "json" };
 const app = express()
 const api = apikey
-const port=3000
+const port = 3000
 console.log("nice!!!!!!!")
 app.get('/', (req, res) => {
     res.send(api)
 })
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("app is running!")
 })
